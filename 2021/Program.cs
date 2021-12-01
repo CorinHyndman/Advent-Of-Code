3f67643b5516aeb4186a8e4f4b652f62fd29ce74
Corin Hyndman <Corin.hyndman@gmail.com>
2021-12-01 18:58:16 +0000

    day one added

---
+++ 2021/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using AOC;
+
+try
+{
+	HelperMethods.Year = 2021;
+	HelperMethods.ExectuingAssembly = Assembly.GetExecutingAssembly();
+
+	DayOne();
+
+
+	Console.ReadLine();
+
+	void DayOne()
+	{
+		int[] input = Array.ConvertAll(
+			array: HelperMethods.GetInput(day: 1).Split(Environment.NewLine),
+			converter: x => int.Parse(x));
+
+		int count = 0;
+		for (int i = 0; i < input.Length - 1;)
+		{
+			if (input[i] < input[++i])
+			{
+				count++;
+			}
+		}
+
+		//Console.WriteLine(count);
+
+		count = 0;
+		int windowSize = 2;
+		int A = input[0] + input[1] + input[2];
+		for (int i = 1; i < input.Length - windowSize; i++)
+		{
+			int B = input[i] + input[i+1] + input[i+2];
+
+			if (B > A)
+			{
+				count++;
+			}
+			
+			A = B;
+		}
+
+		//Console.WriteLine(count);
+	}
+}
+finally
+{
+	Console.ResetColor();
+	Console.Clear();
+}
---
+++ AOC/HelperMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AOC
+{
+	public static class HelperMethods
+	{
+		public static int Year { get; set; }
+		public static Assembly ExectuingAssembly { get; set; }
+		
+		/// <summary>Gets the input data for a specified day</summary>
+		public static string GetInput(int day)
+		{
+			string resourceName = $"_{Year}.Input.{day}.txt";
+
+			string[] embeddedResources = ExectuingAssembly.GetManifestResourceNames();
+			if (!embeddedResources.Contains(resourceName))
+			{
+				Console.WriteLine(@$"Could Not Find ""{resourceName}""");
+				return null;
+			}
+
+			using (var reader = new StreamReader(stream: ExectuingAssembly.GetManifestResourceStream(resourceName)))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
