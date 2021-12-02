2d38ee44341f91ec7dfc6f4443bf5f2c9fcad54e
Corin Hyndman <Corin.hyndman@gmail.com>
2021-12-02 14:08:49 +0000

    added day 2

--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using AOC;
 
@@ -8,12 +9,13 @@ try
 	HelperMethods.ExectuingAssembly = Assembly.GetExecutingAssembly();
 
 	DayOne();
-
+	DayTwo();
 
 	Console.ReadLine();
 
 	void DayOne()
 	{
+		string anwser = null;
 		int[] input = Array.ConvertAll(
 			array: HelperMethods.GetInput(day: 1).Split(Environment.NewLine),
 			converter: x => int.Parse(x));
@@ -27,16 +29,15 @@ try
 			}
 		}
 
-		//Console.WriteLine(count);
+		anwser += $"Part 1: {count} ";
 
 		count = 0;
-		int windowSize = 2;
 		int A = input[0] + input[1] + input[2];
-		for (int i = 1; i < input.Length - windowSize; i++)
+		for (int i = 1; i < input.Length - 2; i++)
 		{
 			int B = input[i] + input[i+1] + input[i+2];
 
-			if (B > A)
+			if (A < B)
 			{
 				count++;
 			}
@@ -44,7 +45,48 @@ try
 			A = B;
 		}
 
-		//Console.WriteLine(count);
+		anwser += $"Part 2: {count}";
+	}
+	void DayTwo()
+	{
+		string[] tmp = HelperMethods.GetInput(day: 2).Split(Environment.NewLine);
+		(string, int)[] input = Enumerable.Range(0, tmp.Length)
+			.Select(i => (tmp[i].Split(' ')[0], int.Parse(tmp[i].Split(' ')[1])))
+			.ToArray();
+
+		int depth = 0;
+		int horizontal = 0;
+		string answer = null;
+		foreach ((string Direction, int Value) in input)
+		{
+			switch (Direction)
+			{
+				case "up": horizontal -= Value; break;
+				case "down": horizontal += Value; break;
+				case "forward": depth += Value; break;
+				default: Console.WriteLine("Invalid Input Detected"); return;
+			}
+		}
+
+		answer += $"Part 1: {horizontal * depth} ";
+
+		int aim = depth = horizontal = 0;
+		foreach ((string Direction, int Value) in input)
+		{
+			switch (Direction)
+			{
+				case "up": aim -= Value; break;
+				case "down": aim += Value; break;
+				case "forward":
+					horizontal += Value;
+					depth += aim * Value;
+					break;
+
+				default: Console.WriteLine("Invalid Input Detected"); return;
+			}
+		}
+
+		answer += $"Part 2: {horizontal * depth}";
 	}
 }
 finally
